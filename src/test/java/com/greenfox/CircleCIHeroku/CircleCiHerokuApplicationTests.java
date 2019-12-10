907a2eac8b21a28645239c1ed7657a93fc52f61d
Pavel Nebesky <pavel@nebesky.eu>
2019-12-10 20:13:23 +0100

    add sum test

--- src/test/java/com/greenfox/CircleCIHeroku/CircleCiHerokuApplicationTests.java
+++ src/test/java/com/greenfox/CircleCIHeroku/CircleCiHerokuApplicationTests.java
@@ -1,13 +1,31 @@
 package com.greenfox.CircleCIHeroku;
 
+import com.greenfox.CircleCIHeroku.services.HomeService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class CircleCiHerokuApplicationTests {
 
+	@Autowired
+	private HomeService homeService;
+
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	void sumWorking(){
+		// given
+		int a = 7;
+		int b = 8;
+
+		// then
+		assertThat(homeService.sumNumbers(a, b))
+				.isEqualTo(15);
 	}
 
 }
