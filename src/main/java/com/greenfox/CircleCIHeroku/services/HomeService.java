0880eb05dd7149d8212708221c7574d90e845f74
Pavel Nebesky <pavel@nebesky.eu>
2019-12-10 16:09:58 +0100

    project structure update

---
+++ src/main/java/com/greenfox/CircleCIHeroku/services/HomeService.java
@@ -0,0 +1,13 @@
+package com.greenfox.CircleCIHeroku.services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class HomeService {
+
+    public int sumNumbers (int a, int b)
+    {
+        return a+b;
+    }
+
+}
---
+++ src/test/java/com/greenfox/CircleCIHeroku/CircleCiHerokuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.greenfox.CircleCIHeroku;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CircleCiHerokuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
