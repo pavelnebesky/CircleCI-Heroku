195f1e0c1022f806c7a33562d9d7ad6c3dc152b0
Pavel Nebesky <pavel@nebesky.eu>
2019-12-10 15:00:12 +0100

    add simple test project

---
+++ CircleCI-Heroku/src/main/java/com/greenfox/CircleCIHeroku/CircleCiHerokuApplication.java
@@ -0,0 +1,13 @@
+package com.greenfox.CircleCIHeroku;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CircleCiHerokuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CircleCiHerokuApplication.class, args);
+	}
+
+}
